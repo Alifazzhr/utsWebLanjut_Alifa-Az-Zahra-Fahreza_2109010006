99a28d6c8475be481c4bff0c3ab0125639470a0e
Alifazzhr <alifazzhr.tata@gmail.com>
2024-04-25 17:50:54 +0700

    jawaban uts nomor 3

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,17 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('produk', function () {
+    $kodeproduk = ['BRG001', 'BRG002'];
+    $namaproduk = ['Buku', 'Pena'];
+    $jenisproduk = ['Alat Tulis', 'Alat Tulis'];
+    $harga = [20000, 15000];
+    $jumlah = count($kodeproduk);
+    return view('produk', compact('kodeproduk', 'namaproduk', 'jenisproduk', 'harga', 'jumlah'));
+});
+
+Route::get('/tambahproduk', function () {
+    $jenisProduk = ['Jenis Produk', 'Alat Tulis', 'Elektronik', 'Sembako'];
+    $jumlahproduk = count($jenisProduk);
+    return view('tambahproduk', compact('jenisProduk', 'jumlahproduk'));
+});
